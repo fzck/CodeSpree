c56e982a7d11926208c408b2f948f2a3c4f131f3
fzck <flzauleck@up.edu.ph>
2017-02-03 23:10:17 +0800

    fixed error
    used the round() function

--- Operators.cpp
+++ Operators.cpp
@@ -10,11 +10,16 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
     
     
-    double mealcost = 0.00;
-    int tip = 0, tax = 0, total = 0;
+    double mealcost = 0.00, total = 0.00;
+    int tip = 0, tax = 0;
     cin >> mealcost >> tip >> tax;
     
-    total = ((mealcost * tip/100) + (mealcost * tax/100) + mealcost);
+    double wtip = (mealcost * tip)/100;
+    double wtap = (mealcost * tax)/100;
+    
+    total =  mealcost + wtip + wtap;
+    
+    total = round(total);
     
     cout << "The total meal cost is "<<total<<" dollars.";
     
